fa49c2e5ee84eb54c6c608d570986241c23c4e61
Tom von Geldern <tvongeldern@Toms-MacBook-Air.local>
2016-06-16 19:23:18 -0500

    cleaned up back end

--- index.js
+++ index.js
@@ -14,163 +14,80 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
+var endpoint = {
+    dbPut: function(endpoint, construct){
+        app.post(endpoint, function(request, response){
+            var query = '';
+            var data = request.body;
+
+            query = construct(data);
+
+            connection.query(query, function(err){
+                if (!err){
+                    response.send({success: true});
+                } else {
+                    response.send({error: err, query: query});
+                }
+            });
+
+        });
+    },
+    dbGet: function(endpoint, construct){
+        app.post(endpoint, function(request, response){
+            var query = '';
+            var data = request.body;
+
+            query = construct(data);
+
+            connection.query(query, function(err, rows){
+                if (!err){
+                    response.send(rows);
+                } else {
+                    response.send({error: err, query: query});
+                }
+            });
+        });
+    }
+};
+
 app.get('/', function(request, response){
     response.sendFile('index.html');
 });
 
-app.post('/userdata', function(request, response){
-
-    var project = request.body.project;
-    var name = request.body.name;
-
-    var query = "SELECT * FROM projects WHERE projectName='" + project + "'";
-
-    connection.query(query, function(err, rows){
-
-        if (!err){
-            response.send(rows);
-        } else {
-            response.send({error: err, query: query});
-        }
-
-    });
-
+endpoint.dbGet('/userdata', function(data){
+    return "SELECT * FROM projects WHERE projectName='" + data.project + "'";
 });
 
-app.post('/createproject', function(request, response){
-
-    var name = request.body.name;
-    var project = request.body.project;
-
-    var query = "INSERT INTO projects (`projectName`, `projectAdmin`) VALUES ('" + project + "', '" + name + "')";
-
-    connection.query(query, function(err){
-        if (!err){
-            response.send({success: true});
-        } else {
-            response.send({error: err, query: query});
-        }
-    });
-
+endpoint.dbPut('/createproject', function(data){
+    return "INSERT INTO projects (`projectName`, `projectAdmin`) VALUES ('" + data.project + "', '" + data.name + "')";
 });
 
-app.post('/projectdata', function(request, response){
-
-    var project = request.body.project;
-
-    var query = "SELECT * FROM rooms WHERE project='" + project + "'";
-
-    connection.query(query, function(err, rows){
-        if (!err){
-            response.send(rows);
-        } else {
-            response.send({error: err, query: query});
-        }
-    });
-
+endpoint.dbGet('/projectdata', function(data){
+    return "SELECT * FROM rooms WHERE project='" + data.project + "'";
 });
 
-app.post('/createroom', function(request, response){
-
-    var project = request.body.project;
-    var roomName = request.body.name;
-
-    var query = "INSERT INTO rooms (`roomName`, `project`) VALUES ('" + roomName + "', '" + project + "')";
-
-    connection.query(query, function(err, rows){
-        if (!err){
-            response.send({success: true});
-        } else {
-            response.send({error: err, query: query});
-        }
-    });
-
+endpoint.dbPut('/createroom', function(data){
+    return "INSERT INTO rooms (`roomName`, `project`) VALUES ('" + data.name + "', '" + data.project + "')";
 });
 
-app.post('/createitem', function(request, response){
-
-    var name = request.body.name;
-    var budget = request.body.budget;
-    var room = request.body.room;
-    var project = request.body.project;
-
-    var query = "INSERT INTO items (`itemName`, `itemBudget`, `itemRoom`, `itemProject`) VALUES ('" + name + "', " + budget + ", '" + room + "', '" + project + "')";
-
-    connection.query(query, function (err){
-
-        if (!err){
-            response.send({success: true});
-        } else {
-            response.send({error: err, query: query});
-        }
-
-    });
-
+endpoint.dbPut('/createitem', function(data){
+    return "INSERT INTO items (`itemName`, `itemBudget`, `itemRoom`, `itemProject`) VALUES ('" + data.name + "', " + data.budget + ", '" + data.room + "', '" + data.project + "')";
 });
 
-app.post('/getitems', function(request, response){
-
-    var project = request.body.project;
-    var room = request.body.room;
-
-    var query = "SELECT * FROM items WHERE itemProject='" + project + "' AND itemRoom='" + room + "'";
-
-    connection.query(query, function(err, rows){
-        if (!err){
-            response.send(rows);
-        } else {
-            response.send({error: err, query: query});
-        }
-    });
-
+endpoint.dbGet('/getitems', function(data){
+    return "SELECT * FROM items WHERE itemProject='" + data.project + "' AND itemRoom='" + data.room + "'";
 });
 
-app.post('/updateitem', function(request, response){
-
-    var item = request.body;
-
-    var query = "UPDATE items SET `itemBudget`=" + item.itemBudget + ", `itemCost`=" + item.itemCost + ", `itemBuyer`=" + item.buyer + " WHERE `itemName`='" + item.itemName + "' AND `itemRoom`='" + item.itemRoom + "' AND `itemProject`='" + item.itemProject + "'";
-
-    connection.query(query, function(err){
-        if (!err){
-            response.send({success: true});
-        } else {
-            response.send({error: err, query: query});
-        }
-    });
-
+endpoint.dbPut('/updateitem', function(data){
+    return "UPDATE items SET `itemBudget`=" + data.itemBudget + ", `itemCost`=" + data.itemCost + ", `itemBuyer`=" + data.buyer + " WHERE `itemName`='" + data.itemName + "' AND `itemRoom`='" + data.itemRoom + "' AND `itemProject`='" + data.itemProject + "'";
 });
 
-app.post('/getbudget', function(request, response){
-
-    var project = request.body.project;
-
-    var query = "SELECT itemBudget, itemCost, itemRoom FROM items WHERE itemProject='" + project + "'";
-
-    connection.query(query, function(err, rows){
-        if (!err){
-            response.send(rows);
-        } else {
-            response.send({error: err, query: query});
-        }
-    });
-
+endpoint.dbGet('/getbudget', function(data){
+    return "SELECT itemBudget, itemCost, itemRoom FROM items WHERE itemProject='" + data.project + "'";
 });
 
-app.post('/deleteitem', function(request, response){
-
-    var item = request.body;
-
-    var query = "DELETE FROM items WHERE `itemProject`='" + item.itemProject + "' AND `itemRoom`='" + item.itemRoom + "' AND `itemName`='" + item.itemName + "'";
-
-    connection.query(query, function(err){
-        if (!err){
-            response.send({success: true});
-        } else {
-            response.send({error: err, query: query});
-        }
-    });
-
+endpoint.dbPut('/deleteitem', function(data){
+    return "DELETE FROM items WHERE `itemProject`='" + data.itemProject + "' AND `itemRoom`='" + data.itemRoom + "' AND `itemName`='" + data.itemName + "'";
 });
 
 app.listen(8080,function(){
